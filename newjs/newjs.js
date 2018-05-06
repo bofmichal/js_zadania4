8bf60b7f99c3816470642fa2323d6b0b30d6b116
bofmichal <bofmichal@gmail.com>
2018-05-06 10:23:50 +1000

    zad1b

--- newjs/newjs.js
+++ newjs/newjs.js
@@ -3,3 +3,13 @@ function zmianatextu1(){
     var zmiana=document.getElementById("text1zm").value;
     document.getElementById("text1").textContent = zmiana;
 }
+function zmianatextu2(){
+    var nowy = document.createElement("span");
+    var zmiana=" "+document.getElementById("text1zm").value;
+var tekst = document.createTextNode(zmiana);
+nowy.appendChild(tekst);
+var rodzic = document.getElementById("text1");
+rodzic.appendChild(nowy);
+    
+    //document.getElementById("text1").innerHTML = pierwszy+zmiana;
+}

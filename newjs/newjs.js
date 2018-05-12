dabae719b6659394f21654f1ba880217ffd214db
bofmichal <bofmichal@gmail.com>
2018-05-12 20:35:24 +1000

    zad2b

--- newjs/newjs.js
+++ newjs/newjs.js
@@ -13,6 +13,7 @@ rodzic.appendChild(nowy);
 }
 
 //zad2
+//a
 function obwod(){
     var boka=parseFloat(document.getElementById("dlugosca").value);
     var bokb=parseFloat(document.getElementById("dlugoscb").value);
@@ -22,4 +23,20 @@ function obwod(){
     }
 wynik=2*boka+2*bokb;
 document.getElementById("suma1").innerHTML=wynik;
+console.log(wynik);
+}
+//b
+function cenabrutto(){
+    var netto=parseFloat(document.getElementById("cenanetto").value);
+    var vat=parseFloat(document.getElementById("vat").value);
+    var brutto;
+    if(isNaN(netto || vat)){
+        alert("wprowadz jako netto i vat liczbę naturalną");
+    }
+    if(vat<0 || vat>100){
+        alert("vat musi zawierać się w przedziale od 0% do 100%");
+    }
+    brutto=(netto*(vat*0.01))+netto;
+    document.getElementById("suma2").innerHTML=brutto;
+    console.log(brutto);
 }
